96dbf3559418e0f7b7d2c5d38b611c43f07b6334
Roy Josefsson <roy.josefsson@etraveligroup.com>
2024-10-15 11:58:19 +0200

    Initial commit

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { styles } from './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <h2>Footer</h2>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/header/Header.tsx
@@ -0,0 +1,11 @@
+import { styles } from './Header.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h2>Header</h2>
+    </header>
+  );
+};
+
+export default Header;
